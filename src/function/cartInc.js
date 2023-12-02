f0c786ebfe13d583aafc0551fa41140ccad944ce
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2023-12-02 18:27:49 +0500

    added size logic

--- src/function/cartInc.js
+++ src/function/cartInc.js
@@ -3,15 +3,34 @@ import { notification } from "antd";
 export default function cartInc(event, item, setCartNum, setQuanNum, itemQuan, size) {
     event.preventDefault();
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    let dupItem = cart.findIndex(cartItem => cartItem.item.clothID === item.clothID);
+    // let dupItemIndex = cart.findIndex(cartItem => cartItem.item.clothID === item.clothID);
+    // let sizeItemIndex = cart.findIndex(cartItem => cartItem.size === size)
 
-    if (dupItem !== -1) {
+    const existingItemIndex = cart.findIndex(cartItem => cartItem.item.clothID === item.clothID && cartItem.size === size);
+
+    if (existingItemIndex !== -1) {
+        // const existingSize = cart[sizeItemIndex]
+        // console.log(existingSize)
+        // if (existingSize === undefined) {
+        //     cart.push({ item, qty: 1, size });
+        // }
+        // else {
+        //     console.log("NO")
+        //     existingSize.qty += itemQuan
+        // }
+
+        // if (itemQuan) {
+        //     cart[dupItemIndex].qty += itemQuan;
+        // } else {
+        //     cart[dupItemIndex].qty += 1;
+        // }
+
         if (itemQuan) {
-            cart[dupItem].qty += itemQuan;
+            cart[existingItemIndex].qty += itemQuan;
         } else {
-            cart[dupItem].qty += 1;
+            cart[existingItemIndex].qty += 1;
         }
-    } else { 
+    } else {
         if (itemQuan) {
             cart.push({ item, qty: itemQuan, size });
         } else {
